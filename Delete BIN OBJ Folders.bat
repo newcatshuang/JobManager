1ecaf666846fc2f101238439e0c8170d25996f5a
newcats <newcats@live.com>
2018-09-20 17:36:31 +0800

    初始化解决方案

---
+++ Delete BIN OBJ Folders.bat
@@ -0,0 +1,9 @@
+@echo off
+@echo Deleting all BIN, OBJ folders...
+for /d /r . %%d in (bin,obj) do @if exist "%%d" rd /s/q "%%d"
+
+@echo.
+@echo BIN and OBJ folders successfully deleted :) Close the window.
+@echo.
+@echo.
+pause > nul
---
+++ src/Newcats.JobManager.Host/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newcats.JobManager.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
