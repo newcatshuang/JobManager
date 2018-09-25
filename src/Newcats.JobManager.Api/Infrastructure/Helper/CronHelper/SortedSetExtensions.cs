66828c5296732cf3ca0e68f507186fb9f650eee5
newcats <newcats@live.com>
2018-09-25 18:39:20 +0800

    增加Cron帮助类

---
+++ src/Newcats.JobManager.Api/Infrastructure/Helper/CronHelper/SortedSetExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Newcats.JobManager.Api.Infrastructure.Helper.CronHelper
+{
+    public static class SortedSetExtensions
+    {
+        public static SortedSet<int> TailSet(this SortedSet<int> set, int value)
+        {
+            return set.GetViewBetween(value, 9999999);
+        }
+    }
+}
---
+++ src/Newcats.JobManager.Api/Infrastructure/Helper/CronHelper/SystemTime.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Newcats.JobManager.Api.Infrastructure.Helper.CronHelper
+{
+    public static class SystemTime
+    {
+        /// <summary>
+        /// Return current UTC time via <see cref="Func{TResult}" />. Allows easier unit testing.
+        /// </summary>
+        public static Func<DateTimeOffset> UtcNow = () => DateTimeOffset.UtcNow;
+
+        /// <summary>
+        /// Return current time in current time zone via <see cref="Func&lt;T&gt;" />. Allows easier unit testing.
+        /// </summary>
+        public static Func<DateTimeOffset> Now = () => DateTimeOffset.Now;
+    }
+}
