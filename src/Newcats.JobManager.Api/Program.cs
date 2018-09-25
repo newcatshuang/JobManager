87c47022f8c5c8ce3d000c3f279429a09790537c
newcats <newcats@live.com>
2018-09-25 18:14:48 +0800

    修改启动配置

--- src/Newcats.JobManager.Api/Program.cs
+++ src/Newcats.JobManager.Api/Program.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace Newcats.JobManager.Api
@@ -19,6 +13,12 @@ namespace Newcats.JobManager.Api
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            .ConfigureLogging((hostingContext, logging) =>
+            {
+                logging.AddFilter("System", LogLevel.Warning);
+                logging.AddFilter("Microsoft", LogLevel.Warning);
+                logging.AddLog4Net();//自动记录全局的异常日志（不需要自己写全局异常过滤器记录）
+            })
+            .UseStartup<Startup>();
     }
 }
--- src/Newcats.JobManager.Api/Startup.cs
+++ src/Newcats.JobManager.Api/Startup.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Newcats.JobManager.Api.Infrastructure.DenpendencyInjection;
 
 namespace Newcats.JobManager.Api
 {
@@ -22,9 +18,26 @@ namespace Newcats.JobManager.Api
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddJsonOptions(options =>
+                {
+                    //忽略循环引用
+                    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+
+                    //不使用驼峰样式的key
+                    options.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver();
+
+                    //设置时间格式
+                    options.SerializerSettings.DateFormatString = "yyyy-MM-dd HH:mm:ss";
+                });
+
+            services.AddHttpContextAccessor();
+
+            //注册Autofac依赖注入服务
+            return services.AddAutofac();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
