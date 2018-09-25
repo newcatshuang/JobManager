34bd3b3caca9d4c35219371e86b88df0aaaebab4
newcats <newcats@live.com>
2018-09-25 15:00:44 +0800

    修改HostServer名称

--- src/Newcats.JobManager.Host/Program.cs
+++ src/Newcats.JobManager.Host/Program.cs
@@ -18,8 +18,8 @@ namespace Newcats.JobManager.Host
                 x.UseLog4Net();
                 x.RunAsLocalSystem();
                 x.Service<ServiceRunner>();
-                x.SetDescription("JobManagerHostServer");
-                x.SetDisplayName("JobManagerHostServer");
+                x.SetDescription("作业调度管理器的托管服务");
+                x.SetDisplayName("作业调度服务");
                 x.SetServiceName("JobManagerHostServer");
             });
         }
