94f9b204ebf3320ff9dcfcf1280c33c541d5b7e2
newcats <newcats@live.com>
2018-10-10 12:18:36 +0800

    更新Delete.bat

--- Delete BIN OBJ Folders.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-@echo Deleting all BIN, OBJ folders...
-for /d /r . %%d in (bin,obj) do @if exist "%%d" rd /s/q "%%d"
-
-@echo.
-@echo BIN and OBJ folders successfully deleted :) Close the window.
-@echo.
-@echo.
-pause > nul
