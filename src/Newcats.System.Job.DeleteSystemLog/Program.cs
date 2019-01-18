19508f855c942e6ca81aa40899bf32871081949c
newcats <newcats@live.com>
2019-01-18 18:51:04 +0800

    更新项目架构

---
+++ src/Newcats.System.Job.DeleteSystemLog/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newcats.System.Job.DeleteSystemLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
