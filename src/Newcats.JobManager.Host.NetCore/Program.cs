eeae08b61fa39825d88a643763e4ffac4e66591b
newcats <newcats@live.com>
2019-01-26 15:05:30 +0800

    添加.net core版本的Host

---
+++ src/Newcats.JobManager.Host.NetCore/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Newcats.JobManager.Host.NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IHostBuilder builder = new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddSingleton<IHostLifetime, TopshelfLifetime>();
+                });
+
+
+        }
+    }
+}
---
+++ src/Newcats.JobManager.Host.NetCore/TopshelfLifetime.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace Newcats.JobManager.Host.NetCore
+{
+    public class TopshelfLifetime : IHostLifetime
+    {
+        private IApplicationLifetime ApplicationLifetime { get; }
+
+        public TopshelfLifetime(IApplicationLifetime applicationLifetime, IServiceProvider serviceProvider)
+        {
+            ApplicationLifetime = applicationLifetime ?? throw new ArgumentNullException(nameof(applicationLifetime));
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task WaitForStartAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
